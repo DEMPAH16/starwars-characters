bd8955c98e638196c485589741a93441d1f812cd
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2018-05-17 20:31:01 -0600

    Added ability to add characters to the app

--- src/App.js
+++ src/App.js
@@ -6,28 +6,118 @@ import Button from './components/Button';
 class App extends Component {
   state = {
     count: 0,
+    firstName: '',
+    lastName: '',
+    affilliation: '',
+    homePlanet: '',
+    characters: [],
   };
   
-  // constructor() {
-  //   super();
-    
-  //   this.addOne = this.addOne.bind(this);
-  // }
+  inputs = [
+    {
+      label: 'First Name',
+      property: 'firstName',
+    },
+    {
+      label: 'Last Name',
+      property: 'lastName',
+    },
+    {
+      label: 'Affilliation',
+      property: 'affilliation',
+    },
+    {
+      label: 'Home Planet',
+      property: 'homePlanet',
+    },
+  ];
   
   render() {
-    // const button = new Button({ text: 'This text comes from App' });
+    const characterList = this.state.characters
+      .map((c, i) => (
+        <li key={`characters-${i}`}>{ c.firstName } { c.lastName } | { c.affilliation } | { c.homePlanet }</li>
+      ));
+    
+    const inputs = this.inputs
+      .map((input, i) => (
+        <div key={`new-character-form-${i}`}>
+          <label>
+            {input.label}:
+            <input
+                type="text"
+                value={this.state[input.property]}
+                onChange={e => this.handleChange(e, input.property)}
+                name={input.property} />
+          </label>
+        </div>
+      ));
     
     return (
       <div className="App">
-        <Button
-          // text={this.state.count}
-          doThisWhenClicked={() => this.addOne()}
+        <form name="add-character-form">
+          {/* <label>
+            Name:
+            <input type="text" value={this.state.name} onChange={e => this.handleChange(e, 'name')} name="name" />
+          </label><br/>
+          
+          <label>
+            Affilliation:
+            <input type="text" value={this.state.affilliation} onChange={e => this.handleChange(e, 'affilliation')} name="affilliation" />
+          </label><br/>
+          
+          <label>
+            Home planet:
+            <input type="text" value={this.state.homePlanet} onChange={e => this.handleChange(e, 'homePlanet')} name="homePlanet" />
+          </label><br/> */}
+          
+          {inputs}
+          
+          <button type="submit" onClick={e => this.handleSubmit(e)}>
+            Submit
+          </button>
+        </form>
+        
+        <ul>
+          {characterList}
+        </ul>
+        
+        {/* <Button
+            // text={this.state.count}
+            doThisWhenClicked={() => this.addOne()}
         >
-          {this.state.count}
+          Count: {this.state.count}
         </Button>
-        {/* <Button /> */}
+        
+        <Button
+            children="this.state.count"
+            doThisWhenClicked={() => console.log('something different')} /> */}
       </div>
     );
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    
+    const { firstName, lastName, affilliation, homePlanet } = this.state;
+    
+    const characters = [
+      ...this.state.characters,
+      { firstName, lastName, affilliation, homePlanet },
+    ];
+    
+    this.setState({
+      characters,
+      firstName: '',
+      lastName: '',
+      affilliation: '',
+      homePlanet: '',
+    });
+  }
+  
+  handleChange(event, name) {
+    // const { target: { value } } = event;
+    const value = event.target.value;
+    this.setState({ [name]: value });
   }
   
   addOne() {
